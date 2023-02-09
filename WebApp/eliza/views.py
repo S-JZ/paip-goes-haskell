89c08a7208d3342694838ec118012403447dbb41
SaachiKaup <saachi.kaup@gmail.com>
2023-02-09 20:11:23 +0530

    Unmerged changes

--- WebApp/eliza/views.py
+++ WebApp/eliza/views.py
@@ -5,6 +5,6 @@ env = environ.Env()
 environ.Env.read_env()
 
 def home(request):
-    return render(request, "eliza.html", context={'eliza_api' : env('ELIZA')})
+    return render(request, "eliza.html", context={'eliza_api' : "https://eliza.azurewebsites.net/api/eliza-main?code=WPDE1bAMb37drOBv2N7nEBuXwf8imucXfTOnl81zshxzAzFuuYLDAA=="})
 
 
--- WebApp/paip/settings.py
+++ WebApp/paip/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+from pickle import FALSE
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +30,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = FALSE
 
 ALLOWED_HOSTS = []
 
